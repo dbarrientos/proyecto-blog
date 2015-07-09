fb54c118a2c0bfe357bf8951eeedb37f27191d52
David Barrientos <david.barrientos.f@gmail.com>
2015-07-09 20:25:15 -0300

    skip token

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: [:confirmation]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
 
   # GET /products
